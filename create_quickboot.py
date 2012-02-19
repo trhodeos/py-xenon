9540771da07a083d199fcbe86dad892535418e52
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2012-02-19 01:10:30 -0600

    Start initial container writing code.

--- create_quickboot.py
+++ create_quickboot.py
@@ -24,28 +24,33 @@ game_name = raw_input("Name of game (eg Skyrim): ")
 game_loc = raw_input("Location of executable (eg \Game\Skyrim\default.xex): ")
 
 dir_name = game_name
+output_dir_name = os.path.join("Content", "0000000000000000", "DEADBEEF", "00007000")
+output_name = os.path.join(output_dir_name, game_name)
 
-# create directory structure
-print "Creating files...",
-os.mkdir(dir_name)
-
-# copy in default.xex
 xex_name = os.path.join(dir_name, "default.xex")
 to_be_copied = os.path.join(data_dir_name, "default.xex")
+
+config_name = os.path.join(dir_name, "config.ini")
+
+print "Creating files...",
+# create directory structure
+os.mkdir(dir_name)
+os.makedirs(output_dir_name)
+
+# copy in default.xex
 shutil.copy(to_be_copied, xex_name)
 
 # create config file
-config_name = os.path.join(dir_name, "config.ini")
 config_file = open(config_name, "w")
 config_file.write(game_loc+"\n")
 config_file.close()
-
 print "Done"
 
 # create LIVE container for directory structure
 print "Creating container...",
 container = containers.LiveContainer()
-
+container.set_title(game_name)
+container.write(output_name)
 print "Done"
 
 # clean up
--- xenon/containers.py
+++ xenon/containers.py
@@ -1,2 +1,50 @@
+"""
+All information used to construct these containers can be found here: http://free60.org/old/Generic_Xbox_360_File_Container.html
+"""
+
 class LiveContainer:
-    pass
+    def set_directory(self, dir):
+        self.directory = dir
+
+    def set_title(self, title):
+        if (len(title) > 256):
+            title = title[:256]
+        assert(len(title) < 256)
+        self.title = title
+
+    def write_header(self, file):
+        """
+        Write the header information (minus any hashes) to file.
+        file - file to write into.
+        """
+        # magic int
+        file.seek(0x0)
+        file.write('LIVE')
+
+        # content type
+        file.seek(0x344)
+        file.write("\x00\x00\x70\x00") # XNA Community / xbox title
+
+        # title
+        file.seek(0x410)
+        file.write(self.title)
+
+        # icons
+        file.seek(0x1712) # length of big icon
+        file.write("\x00\x00\x00\x00")
+
+        file.seek(0x1716) # length of small icon
+        file.write("\x00\x00\x00\x00")
+
+
+    def write(self, filename):
+        # open up the file
+        file = open(filename, 'wb')
+
+        # write header info
+        self.write_header(file)
+
+        # write content info
+
+        # close up shop
+        file.close()
